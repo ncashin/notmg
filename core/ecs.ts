3fad43487553accea82d85696df831e8a325e825
NatalieC2k <nataliec2k5@gmail.com>
2025-03-17 13:17:17 -0230

    cleanup

--- core/ecs.ts
+++ core/ecs.ts
@@ -5,101 +5,113 @@ type Component = { type: string };
 let componentPools: Record<string, Component[]> = {};
 let composedPools: Record<string, Component[][]> = {};
 
-let toUpdateOnChange: Record<string, string[]> = {};
+let associatedComposedPoolKeys: Record<string, string[]> = {};
 
 export const createEntity = (): Entity => {
   return ENTITY_ID++;
+};
+
+const lookupComponent = <ComponentType extends Component>(
+  entity: Entity,
+  COMPONENT_TYPE_DEF: ComponentType
+) => {
+  if (componentPools[COMPONENT_TYPE_DEF.type] === undefined) {
+    componentPools[COMPONENT_TYPE_DEF.type] = [];
+  }
+  return componentPools[COMPONENT_TYPE_DEF.type][entity] as ComponentType;
+};
+const createComponentReference = <ComponentType extends Component>(
+    entity: Entity,
+    COMPONENT_TYPE_DEF: ComponentType
+  ) => {
+    if (componentPools[COMPONENT_TYPE_DEF.type] === undefined) {
+      componentPools[COMPONENT_TYPE_DEF.type] = [];
+    }
+    componentPools[COMPONENT_TYPE_DEF.type][entity] = structuredClone(COMPONENT_TYPE_DEF);
+  };
+const lookupAssociatedComposedPoolKeys = <ComponentType extends Component>(
+  COMPONENT_TYPE_DEF: ComponentType
+) => {
+  if (associatedComposedPoolKeys[COMPONENT_TYPE_DEF.type] === undefined) {
+    associatedComposedPoolKeys[COMPONENT_TYPE_DEF.type] = [];
+  }
+  return associatedComposedPoolKeys[COMPONENT_TYPE_DEF.type];
 };
 
 export const getComponent = <ComponentType extends Component>(
   entity: Entity,
   COMPONENT_TYPE_DEF: ComponentType
 ): ComponentType => {
-  if (componentPools[COMPONENT_TYPE_DEF.type] === undefined) {
-    componentPools[COMPONENT_TYPE_DEF.type] = [];
-  }
-  return componentPools[COMPONENT_TYPE_DEF.type][entity] as ComponentType;
+  return lookupComponent(entity, COMPONENT_TYPE_DEF);
 };
 export const addComponent = <ComponentType extends Component>(
   entity: Entity,
   COMPONENT_TYPE_DEF: ComponentType
 ) => {
-  console.log(toUpdateOnChange);
+  createComponentReference(entity, COMPONENT_TYPE_DEF);
 
-  let newComponent = structuredClone(COMPONENT_TYPE_DEF);
-  if (componentPools[COMPONENT_TYPE_DEF.type] === undefined) {
-    componentPools[COMPONENT_TYPE_DEF.type] = [];
-  }
-
-  componentPools[COMPONENT_TYPE_DEF.type][entity] = newComponent;
-
-  if (toUpdateOnChange[COMPONENT_TYPE_DEF.type] === undefined) {
-    toUpdateOnChange[COMPONENT_TYPE_DEF.type] = [];
-  }
-  toUpdateOnChange[COMPONENT_TYPE_DEF.type].forEach((keyToUpdate) => {
-    const parsedKeyComponentTypes = keyToUpdate.split(" ");
-
-    let composedComponents = parsedKeyComponentTypes.reduce(
-      (composedArray, componentType) => [
-        ...composedArray,
-        getComponent(entity, { type: componentType }),
-      ],
-      [] as Component[]
-    );
-
-    if (
-      composedComponents.filter((a) => a).length ===
-      parsedKeyComponentTypes.length
-    ) {
+  lookupAssociatedComposedPoolKeys(COMPONENT_TYPE_DEF).forEach(
+    (keyToUpdate) => {
+      const parsedKeyComponentTypes = keyToUpdate.split(" ");
+      let composedComponents = [];
+      for (let i = 0; i < parsedKeyComponentTypes.length; i++) {
+        let component = lookupComponent(entity, {
+          type: parsedKeyComponentTypes[i],
+        });
+        if (component === undefined) return;
+        composedComponents.push(component);
+      }
       composedPools[keyToUpdate][entity] = composedComponents;
     }
-  });
+  );
 };
 export const removeComponent = <ComponentType extends Component>(
   entity: Entity,
   COMPONENT_TYPE_DEF: ComponentType
 ) => {
-  if (toUpdateOnChange[COMPONENT_TYPE_DEF.type] === undefined) {
-    toUpdateOnChange[COMPONENT_TYPE_DEF.type] = [];
-  }
-  toUpdateOnChange[COMPONENT_TYPE_DEF.type].forEach((keyToUpdate) => {
-    delete composedPools[keyToUpdate][entity];
-  });
+  lookupAssociatedComposedPoolKeys(COMPONENT_TYPE_DEF).forEach(
+    (keyToUpdate) => {
+      delete composedPools[keyToUpdate][entity];
+    }
+  );
   delete componentPools[COMPONENT_TYPE_DEF.type][entity];
 };
 
-export const queryEntities = <ComposedType extends Component[]>(
+export const queryEntities = <const ComposedType extends Component[]>(
   COMPONENT_TYPE_DEFS: ComposedType,
   lambda: (arg0: ComposedType) => void
 ) => {
+  if (COMPONENT_TYPE_DEFS.length == 1) {
+  }
+
   const combination = COMPONENT_TYPE_DEFS.map(
     (COMPONENT_TYPE_DEF) => COMPONENT_TYPE_DEF.type
   ).reduce((previous, current) => previous + " " + current);
 
   if (composedPools[combination] === undefined) {
     let poolComponents: Component[][] = [];
+
     // This can be fixed so that not all entities need to be looped through this is for testing
-    for (let i = 0; i < ENTITY_ID; i++) {
-      let composedComponents = COMPONENT_TYPE_DEFS.reduce(
-        (composedArray, COMPONENT_TYPE_DEF) => [
-          ...composedArray,
-          getComponent(i, COMPONENT_TYPE_DEF),
-        ],
-        [] as Component[]
+    for (let entityID = 0; entityID < ENTITY_ID; entityID++) {
+      const componentTypes = COMPONENT_TYPE_DEFS.map(
+        (COMPONENT_TYPE_DEF) => COMPONENT_TYPE_DEF.type
       );
-      if (
-        composedComponents.filter((a) => a).length !==
-        COMPONENT_TYPE_DEFS.length
-      ) {
+      let composedComponents = [];
+      for (let i = 0; i < componentTypes.length; i++) {
+        let component = lookupComponent(entityID, {
+          type: componentTypes[i],
+        });
+        if (component === undefined) break;
+        composedComponents.push(component);
+      }
+
+      if (composedComponents.length < componentTypes.length) {
         continue;
       }
-      poolComponents[i] = composedComponents;
+      poolComponents[entityID] = composedComponents;
 
       COMPONENT_TYPE_DEFS.forEach((COMPONENT_TYPE_DEF) => {
-        if (toUpdateOnChange[COMPONENT_TYPE_DEF.type] === undefined) {
-          toUpdateOnChange[COMPONENT_TYPE_DEF.type] = [];
-        }
-        toUpdateOnChange[COMPONENT_TYPE_DEF.type].push(combination);
+        lookupAssociatedComposedPoolKeys(COMPONENT_TYPE_DEF).push(combination);
       });
     }
     composedPools[combination] = poolComponents;
