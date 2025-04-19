ab04cea4afd9c4e0f849d6e63102292eeb6e357e
NatalieC2k <nataliec2k5@gmail.com>
2025-04-19 15:12:51 -0230

    started doing networking things

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,7 @@ import { draw } from "./draw";
 import { inputMap } from "./input";
 import { provideECSInstanceFunctions } from "../../core/ecs";
 import {
-  AABB_COLLIDER_COMPONENT_DEF,
-  createKinematicEntity,
   POSITION_COMPONENT_DEF,
-  updateCollisionSystem,
-  VELOCITY_COMPONENT_DEF,
 } from "../../core/collision";
 
 export const {
@@ -32,50 +28,6 @@ export const updateObjectRecursive = (object: any, objectToUpdateWith: any) => {
   });
 };
 
-<<<<<<< HEAD
-let position = getComponent(playerEntity, POSITION_COMPONENT_DEF);
-let velocity = getComponent(playerEntity, VELOCITY_COMPONENT_DEF);
-let color = getComponent(playerEntity, COLOR_COMPONENT_DEF);
-
-position.x = 100;
-position.y = 100;
-color.color = "blue";
-
-const movementTestEntity = createKinematicEntity(ecsInstance);
-addComponent(movementTestEntity, COLOR_COMPONENT_DEF);
-
-let moveTestPosition = getComponent(movementTestEntity, POSITION_COMPONENT_DEF);
-let moveTestVelocity = getComponent(movementTestEntity, VELOCITY_COMPONENT_DEF);
-let moveTestColor = getComponent(movementTestEntity, COLOR_COMPONENT_DEF);
-
-moveTestPosition.x = 1000;
-moveTestPosition.y = 300;
-moveTestColor.color = "green";
-
-//testCollider
-let testCollisionEntity = createEntity();
-addComponent(testCollisionEntity, POSITION_COMPONENT_DEF);
-addComponent(testCollisionEntity, AABB_COLLIDER_COMPONENT_DEF);
-addComponent(testCollisionEntity, COLOR_COMPONENT_DEF);
-
-//testCollider
-testCollisionEntity = createEntity();
-
-addComponent(testCollisionEntity, POSITION_COMPONENT_DEF);
-addComponent(testCollisionEntity, AABB_COLLIDER_COMPONENT_DEF);
-addComponent(testCollisionEntity, COLOR_COMPONENT_DEF);
-
-let testPosition = getComponent(testCollisionEntity, POSITION_COMPONENT_DEF);
-let testCollider = getComponent(
-  testCollisionEntity,
-  AABB_COLLIDER_COMPONENT_DEF,
-);
-
-testPosition.x = 500;
-testPosition.y = 700;
-testCollider.width = 1000;
-testCollider.height = 64;
-=======
 const websocket = new WebSocket("ws://localhost:3000");
 websocket.onopen = () => {
   console.log("Connected to WebSocket server");
@@ -100,7 +52,6 @@ websocket.onerror = (error) => {
 websocket.onclose = () => {
   console.log("Disconnected from WebSocket server");
 };
->>>>>>> 78a1fa6 (oops)
 
 const DAMPING_FORCE = 0.2;
 const PLAYER_SPEED = 1;
