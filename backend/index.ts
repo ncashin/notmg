4b02d7206365143e3385a837987a3c6745b03358
NatalieC2k <nataliec2k5@gmail.com>
2025-04-19 16:30:25 -0230

    I am genuinely astonished this works

--- backend/index.ts
+++ backend/index.ts
@@ -24,17 +24,71 @@ export const {
 } = provideECSInstanceFunctions();
 
 type WebSocketData = {
-  entityID: number;
+  entity: number;
 };
 let connectedSockets: Array<ServerWebSocket<WebSocketData>> = [];
-const sendUpdatedComponent = (entity: Entity, component: Component) => {
+
+export function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+export function mergeDeep(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
+
+let catchupPacket: any = {};
+let updatePacket: any = {};
+const sendUpdatePacket = () => {
   connectedSockets.forEach((socket) => {
-    socket.send(entity.toString() + "*" + JSON.stringify(component));
+    socket.send(
+      JSON.stringify({
+        type: "update",
+        packet: updatePacket,
+      })
+    );
+  });
+  mergeDeep(catchupPacket, updatePacket);
+  updatePacket = {};
+};
+export const getNetworkedComponent = <T extends Component>(
+  entity: Entity,
+  component: T
+) => {
+  return new Proxy(getComponent(entity, component), {
+    set: (
+      target: any,
+      property: string | symbol,
+      newValue: any,
+      _receiver: any
+    ) => {
+      if (updatePacket[entity] === undefined) updatePacket[entity] = {};
+      if (updatePacket[entity][component.type] === undefined)
+        updatePacket[entity][component.type] = {};
+      updatePacket[entity][component.type][property] = newValue;
+      target[property] = newValue;
+      return true;
+    },
   });
 };
 export const addNetworkedComponent = (entity: Entity, component: Component) => {
+  if (updatePacket[entity] === undefined) updatePacket[entity] = {};
+  if (updatePacket[entity][component.type])
+    updatePacket[entity][component.type] = {};
+  updatePacket[entity][component.type] = component;
   addComponent(entity, component);
-  sendUpdatedComponent(entity, component);
 };
 
 const server = Bun.serve<WebSocketData, {}>({
@@ -43,7 +97,7 @@ const server = Bun.serve<WebSocketData, {}>({
     if (
       server.upgrade(req, {
         data: {
-          entityID: createEntity(),
+          entity: createEntity(),
         },
       })
     ) {
@@ -53,29 +107,34 @@ const server = Bun.serve<WebSocketData, {}>({
   },
   websocket: {
     open(websocket) {
-      console.log("Client connected");
-
-      connectedSockets.push(websocket);
-      addNetworkedComponent(websocket.data.entityID, POSITION_COMPONENT_DEF);
-      addNetworkedComponent(websocket.data.entityID, VELOCITY_COMPONENT_DEF);
-      addNetworkedComponent(
-        websocket.data.entityID,
-        AABB_COLLIDER_COMPONENT_DEF
+      websocket.send(
+        JSON.stringify({
+          type: "initialization",
+          playerEntity: websocket.data.entity,
+          catchupPacket: catchupPacket,
+        })
       );
+      connectedSockets.push(websocket);
+      addNetworkedComponent(websocket.data.entity, POSITION_COMPONENT_DEF);
+      addNetworkedComponent(websocket.data.entity, VELOCITY_COMPONENT_DEF);
+      addNetworkedComponent(websocket.data.entity, AABB_COLLIDER_COMPONENT_DEF);
     },
     message(websocket, message) {
       if (typeof message !== "string") return;
       const [x, y] = message.split(" ").map(Number.parseFloat);
-      let position = getComponent(
-        websocket.data.entityID,
+      let position = getNetworkedComponent(
+        websocket.data.entity,
         POSITION_COMPONENT_DEF
       );
       position.x = x;
       position.y = y;
-      sendUpdatedComponent(websocket.data.entityID, position);
     },
     close(_websocket) {},
   },
 });
+
+setInterval(() => {
+  sendUpdatePacket();
+}, 1000);
 
 console.log(`WebSocket server listening on port ${server.port}`);
