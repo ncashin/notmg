e5fc5e6c99ca5b2a9d37db724d2dc2e014da0720
Natalie Cashin <nataliec2k5@gmail.com>
2025-07-03 20:15:49 -0230

    this still needs productionization

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,12 @@
 import { Database } from "bun:sqlite";
+import { mkdir } from "node:fs/promises";
+import { dirname } from "node:path";
 import { drizzle } from "drizzle-orm/bun-sqlite";
+
+const databaseDirectory = dirname("database/sqlite.db");
+try {
+  await mkdir(databaseDirectory, { recursive: true });
+} catch (error) {}
 
 const sqlite = new Database("database/sqlite.db");
 export const database = drizzle({ client: sqlite });
