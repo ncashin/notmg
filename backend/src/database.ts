ecceb78f1b7710404c8ed213b80f4c88a474a31b
Natalie Cashin <nataliec2k5@gmail.com>
2025-07-03 20:19:14 -0230

    migrate on startup

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,6 +2,7 @@ import { Database } from "bun:sqlite";
 import { mkdir } from "node:fs/promises";
 import { dirname } from "node:path";
 import { drizzle } from "drizzle-orm/bun-sqlite";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 const databaseDirectory = dirname("database/sqlite.db");
 try {
@@ -10,3 +11,5 @@ try {
 
 const sqlite = new Database("database/sqlite.db");
 export const database = drizzle({ client: sqlite });
+
+await migrate(database, { migrationsFolder: "./migrations" });
