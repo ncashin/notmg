3a6c6dcce34ab2ef759beecd9096ee4b1044d57c
Natalie Cashin <nataliec2k5@gmail.com>
2025-07-03 20:12:03 -0230

    db dir change

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,5 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database("database/sqlite.db");
 export const database = drizzle({ client: sqlite });
